8bde6284e52effc73c5e6913d26852759796d59b
unknown <hanadi.hosri@gmail.com>
2024-03-03 21:28:39 +0200

    adding event listener to tile clicks

--- js/main.js
+++ js/main.js
@@ -1,25 +1,41 @@
 const colorsOrder = [];
 const start = document.getElementById("play");
+const greenTile = document.querySelector(".tile.green.inactive");
+const redTile = document.querySelector(".tile.red.inactive");
+const blueTile = document.querySelector(".tile.blue.inactive");
+const yellowTile = document.querySelector(".tile.yellow.inactive");
 
 
 start.addEventListener("click", function() {
     addRandomColor();
     changeColorOpacity()
+    getSound();
+});
+
+greenTile.addEventListener("click", function() {
+    handleTileClick("green");
+});
+
+redTile.addEventListener("click", function() {
+    handleTileClick("red");
+});
+
+blueTile.addEventListener("click", function() {
+    handleTileClick("blue");
+});
+
+yellowTile.addEventListener("click", function() {
+    handleTileClick("yellow");
 });
 
 function addRandomColor() {
     const colors = ["red", "green", "blue", "yellow"];
-    const randomColor = colors[Math.floor(Math.random()*3)];
+    const randomColor = colors[Math.floor(Math.random()*4)];
     colorsOrder.push(randomColor);
     
 };
 
 function changeColorOpacity() {
-    const greenTile = document.querySelector(".tile.green.inactive");
-    const redTile = document.querySelector(".tile.red.inactive");
-    const blueTile = document.querySelector(".tile.blue.inactive");
-    const yellowTile = document.querySelector(".tile.yellow.inactive");
-
     for (let i = 0; i < colorsOrder.length; i++) {
         if (colorsOrder[i] == "green") {
             greenTile.style.opacity = 1;
@@ -44,6 +60,11 @@ function changeColorOpacity() {
             setTimeout(() => {
                 yellowTile.style.opacity = 0.35;
             }, 2000);
-        }
-    }
+        };
+    };
 };
+
+function getSound() {
+
+}
+
