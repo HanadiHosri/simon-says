f568ab56fbd72f367d1996e9d397f2c761ebf890
unknown <hanadi.hosri@gmail.com>
2024-03-03 20:02:07 +0200

    adding addRandomColor function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-const sequence = [];
+const colorsOrder = [];
+const start = document.getElementById("play");
+
+
+start.addEventListener("click", function() {
+    addRandomColor();
+})
+
+function addRandomColor() {
+    const colors = ["red", "green", "blue", "yellow"];
+    const randomColor = colors[Math.floor(Math.random()*3)];
+    colorsOrder.push(randomColor);
+}
